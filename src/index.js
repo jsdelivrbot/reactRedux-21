5e848d18c08f560ec30578d1ce0462bb8ebcfcb7
bronzlabec <aurelian.ardeleanu@gmail.com>
2018-03-16 17:15:20 +0200

    lecture 1.11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce some HTML.
-const App = function() {
+const App = () => {
     return <div>Hi!</div>;
 }
 
 // Take the HTML the component generated and put it on the page (in the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
